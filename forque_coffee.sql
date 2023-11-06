fdeba183e2fe33c692df7b65de1810b846da3ce2
Ran Ran <43269555+rayfarandi@users.noreply.github.com>
2023-11-06 22:39:38 +0700

    tugas

--- forque_coffee.sql
+++ forque_coffee.sql
@@ -40,6 +40,27 @@ insert into "product" ("name_product","price","category","description_product","
 values ('Artic Monkey',24000,'coffee','coffee,milk,brownSugar,creamLate',50),
 		('Bolt Thrower',28000,'coffee','strongCoffee,milk,smoothFoam',50);
 
+insert into "promo" ("name_promo","min_purches","max_discount","discount_percentage")
+values ('NovemberWEEK',50000,10000,50.00);
+
+insert into "user" ("username","email","password","full_name","birthday","phone_number")
+values ('ran1','ran1@gmail.com','fazztrack','ranran','1994-02-14','877777756');
+
+insert into "user" ("username","email","password","full_name","birthday","phone_number")
+values ('ran','ran@gmail.com','fazztrack','ranran','1994-02-14','0877777756');
+
+ALTER TABLE public."user" ALTER COLUMN "phone_number" TYPE varchar(15) USING phone_number::varchar;
+
+delete from public."user" where "user_id" = 1; 
+
+insert into "order" ("user_id","total_amount","status")
+values ('2','70000','selesa');
+
+
+
+
+
+
 
  
  
